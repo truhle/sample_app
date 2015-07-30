e7a9d4cc400eb47ea2ac53e0f179f1557b457d6e
Todd Ruhlen <rhythmtome@yahoo.com>
2015-07-30 13:26:32 -0700

    Finish Exercises

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,6 +11,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
   end
 
   test "valid signup information" do
@@ -23,5 +25,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       }
     end
     assert_template 'users/show'
+    assert_not flash.empty?
   end
 end
